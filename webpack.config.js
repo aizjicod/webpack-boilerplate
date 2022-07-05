859cb7aa32b99f4aa418a1560c3905d6a0ed70e8
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-07-05 13:29:08 -0400

    add live server

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  }, 
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   module: {
     rules: [
       {
@@ -19,5 +20,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
-  ]
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }, 
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // }
 };
